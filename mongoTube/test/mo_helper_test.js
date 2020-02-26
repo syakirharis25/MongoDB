e093eeb186d06fddb863532df1487f37c6905f8e
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-27 04:28:26 +0800

    modified:   mongoTube/test/mo_helper_test.js

--- mongoTube/test/mo_helper_test.js
+++ mongoTube/test/mo_helper_test.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/mongotube", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-mongoose.connection
-    .once('open', () => console.log('Connected'))
-    .on('error', (error) => {
-        console.log("Your Error", error);
+before(done => {
+    mongoose.connect("mongodb://localhost/mongotube", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
+
+    mongoose.connection
+        .once('open', () => {
+            // console.log('Connected');
+            done();
+        })
+        .on('error', (error) => {
+            console.log("Your Error", error);
+        });
+});
