72a6f0e211a7a4caef6c19d5e4b468c322f3e430
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-27 04:51:43 +0800

    modified:   mongoTube/package.json         new file:   mongoTube/test/mo_create_test.js

---
+++ mongoTube/test/mo_create_test.js
@@ -0,0 +1,8 @@
+const Student = require('../app/student');
+const assert = require('assert');
+
+describe('Create records', () => {
+    it('create a user in DB', () => {
+        // assert(false);
+    });
+});
