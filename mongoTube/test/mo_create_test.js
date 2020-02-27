f003293f4b8a4caee39d304da164110ef639ee38
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-27 09:22:35 +0800

    modified:   mongoTube/test/mo_create_test.js

--- mongoTube/test/mo_create_test.js
+++ mongoTube/test/mo_create_test.js
@@ -1,7 +1,7 @@
 const Student = require("../app/student");
 const assert = require("assert");
 
-describe("Create records", () => {
+describe("Create Tests", () => {
     it("create a user in DB", () => {
         // assert(false);
         const sam = new Student({ name: "Sam" });
@@ -14,3 +14,64 @@ describe("Create records", () => {
             })
     });
 });
+
+// All read tests
+
+describe("Read Tests", () => {
+    let reader;
+    beforeEach((done) => {
+        reader = new Student({ name: "Reader" })
+        reader.save()
+            .then(() => {
+                done();
+            })
+    })
+    it("Read a user: Reader", (done) => {
+        Student.find({ name: "Reader" })
+            .then((students) => {
+                // id is a BSON value
+                assert(reader._id.toString() === students[0]._id.toString());
+                done();
+            });
+    });
+});
+
+// All delete test
+
+describe("Delete Tests", () => {
+    let deleter;
+
+    beforeEach((done) => {
+        deleter = new Student({ name: "Deleter" })
+        deleter.save().then(() => done());
+    });
+
+    it("A delete test for deleter", done => {
+        Student.findByIdAndDelete(deleter._id)
+            .then(() => Student.findOne({ name: "Deleter" }))
+            .then((student) => {
+                assert(student === null);
+                done();
+            });
+    });
+});
+
+// All update test
+
+describe('Update Tests', () => {
+    let updater;
+    beforeEach((done) => {
+        updater = new Student({ name: 'Updater' })
+        updater.save()
+            .then(() => done())
+    })
+    it('Set n Save test', () => {
+        updater.set('name', "UpUpdater");
+        updater.save()
+            .then(() => Student.find({}))
+            .then(students => {
+                assert(students[0].name !== 'Updater');
+                // assert(students[0].name === 'UpUpdater');
+            })
+    })
+})
